19844f6b8cb52fcd9290d8e44537d98b02e3d29c
jburton321 <jburton.mail10@yahoo.com>
2026-01-29 10:57:43 -0500

    Updated tailwind.css

--- src/components/AmenitiesGrid.tsx
+++ src/components/AmenitiesGrid.tsx
@@ -11,7 +11,7 @@ const amenities = [
 
 export function AmenitiesGrid() {
   return (
-    <section className="py-10 sm:py-12 md:py-16 px-4 sm:px-6">
+    <section className="pt-0 pb-10 sm:pb-12 md:pb-16 px-4 sm:px-6">
       <div className="max-w-content mx-auto text-center">
         <img
           className="h-10 sm:h-12 md:h-14 mx-auto mb-6 sm:mb-8"
