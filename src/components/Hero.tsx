4b71a78103f84ac97a1b84939e7a7ab9cf95a0e5
jburton321 <jburton.mail10@yahoo.com>
2026-01-29 17:22:27 -0500

    Updated tailwind.css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero() {
         backgroundImage="section0.png"
         overlay
         overlayOpacity={30}
-        className="relative min-h-[500px] md:min-h-[600px] lg:min-h-[700px] xl:min-h-[800px] flex flex-col overflow-visible"
+        className="relative min-h-[500px] md:min-h-[600px] lg:min-h-[800px] flex flex-col overflow-visible"
       >
         <div className="max-w-content mx-auto px-4 md:px-6 lg:px-8 pt-fluid-3 w-full flex-1 flex flex-col">
           <div className="flex flex-col lg:flex-row lg:justify-between lg:items-start gap-fluid-4 flex-1">
