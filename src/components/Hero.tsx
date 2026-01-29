0eedb53d10eacb168bc8c8a689372f075f723932
jburton321 <jburton.mail10@yahoo.com>
2026-01-29 17:17:41 -0500

    Updated tailwind.css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -42,7 +42,7 @@ export function Hero() {
                 </button>
               </div>
             </div>
-            <div className="w-full lg:w-auto flex justify-center lg:justify-end order-2 -mx-4 md:-mx-6 lg:mx-0 lg:px-0">
+            <div className="w-[calc(100%+2rem)] md:w-[calc(100%+3rem)] lg:w-auto -mx-4 md:-mx-6 lg:mx-0 order-2">
               <BookingCard />
             </div>
           </div>
